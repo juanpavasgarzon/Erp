61775a03fdaafb848c30b677f3a54df9a17f76da
Juan Pavas <garzonp2001@gmail.com>
2024-04-16 12:28:28 -0500

    employees

--- Pavas.Domain/Executors/Inventory/Commands/Subtract/SubtractInventoryCommand.cs
+++ Pavas.Domain/Executors/Inventory/Commands/Subtract/SubtractInventoryCommand.cs
@@ -2,7 +2,4 @@ using Pavas.Abstractions.Dispatch.Commands.Contracts;
 
 namespace Pavas.Domain.Executors.Inventory.Commands.Subtract;
 
-public record SubtractInventoryCommand(
-    int InventoryId,
-    decimal Quantity
-) : ICommand;
+public record SubtractInventoryCommand(int InventoryId, decimal Quantity) : ICommand;
